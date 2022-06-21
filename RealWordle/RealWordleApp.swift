4444dbb2e036b91fa18d936b2581a380633d57c5
Troy Hurst <troy.hurst@icloud.com>
2022-06-21 10:17:20 -0500

    Initial Commit

---
+++ RealWordle/RealWordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealWordleApp.swift
+//  RealWordle
+//
+//  Created by Troy Hurst on 6/21/22.
+//
+
+import SwiftUI
+
+@main
+struct RealWordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
